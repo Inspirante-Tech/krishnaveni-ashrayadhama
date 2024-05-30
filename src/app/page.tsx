984bc3b4cfaf80e587a70210145803f7ec1805b9
VarshithPawarHR <varshithpawarhr@gmail.com>
2024-05-31 00:21:48 +0530

    footer changes as per design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
-
-
-import Hero from "@components/Hero/Hero"
+import Hero from "@components/Hero/Hero";
 import Story from "@components/Information/Story";
 import Information from "@components/Information/Information";
 import Facilities from "@components/Information/Facilities";
 import Testimonials from "~/components/Testimonials/page";
-
-
 
 export default function Home() {
   return (
     <>
       <Hero />
       <main className="w-full px-6 md:px-40">
-      <Story />
-      <Facilities/>
-      <Information/>
-     </main>
+        <Story />
+        <Facilities />
+        <Information />
+      </main>
       <Testimonials />
     </>
-
   );
 }
