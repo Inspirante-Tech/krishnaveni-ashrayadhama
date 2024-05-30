23229457a4e6bb6e1eadf72f38ae44e27b9262ea
Sathwik <hsathwik002@gmail.com>
2024-05-30 20:07:44 +0530

    fix:Hero_section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,25 @@
+import { HeroimageUrls } from "~/constants";
 import Carousel from "./Carousel";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 function Hero() {
-    return (
-        <Carousel>
-            <img className="min-w-[100vw]
-            h-full object-cover snap-center"
-                src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-            />
-            <img className="min-w-[100vw] h-full object-cover snap-center"
-                src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            />
-            <img className="min-w-[100vw] h-full object-cover snap-center"
-                src="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-            />
-            <img className="min-w-[100vw] h-full object-cover snap-center"
-                src="https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-            />
-            <img className="min-w-[100vw] h-full snap-center"
-                src="https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-            />
-
-        </Carousel>
-    )
+  return (
+    <div className="">
+      <Carousel>
+        {HeroimageUrls.map((url, index) => (
+          <Image
+            key={index}
+            width={100}
+            height={100}
+            className="min-w-[100vw] h-full object-cover snap-center"
+            src={url}
+            alt={`Slide ${index + 1}`}
+          />
+        ))}
+      </Carousel>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -258,4 +258,11 @@ export const questionAndAnswers = [
       ' available, ected humour,ised words which don\'t look even slightly believable.',
   },
 ];
+ export const HeroimageUrls = [
+  "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+];
 
