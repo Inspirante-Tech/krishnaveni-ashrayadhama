794efc594ede2b61bb314d17727e43e81695bdb3
VarshithPawarHR <varshithpawarhr@gmail.com>
2024-06-01 19:28:13 +0530

    added hover animation to navbar --- could not fix the issue of icon in mobile format

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import { navigation } from "~/constants";
 import Button from "../design/Button";
 import MenuSvg from "~/assets/svg/MenuSvg";
+import styles from "./styles.module.css";
 
 const Header = () => {
   const [openNavigation, setOpenNavigation] = useState(false);
@@ -18,19 +19,23 @@ const Header = () => {
   }, []);
 
   return (
-    <div className={`fixed top-0 left-0 w-full z-50   lg:bg-n-8/90 lg:backdrop-blur-md ${openNavigation ? "bg-primary-50" : "bg-blur backdrop-blur-md "}`}>
+    <div
+      className={`fixed top-0 left-0 w-full z-50 lg:bg-n-8/90 lg:backdrop-blur-md ${openNavigation ? "bg-primary-50" : "bg-blur backdrop-blur-md"}`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="/">
           <h1 className="font-bold text-3xl">Logo</h1>
         </a>
 
-        <nav className={`${openNavigation ? "flex bg-primary-50  " : "hidden"} fixed top-[5rem] left-0 right-0 bottom-0 lg:bg-blur lg:backdrop-blur lg:static lg:flex lg:mx-auto `}>
+        <nav
+          className={`${openNavigation ? "flex bg-primary-50" : "hidden"} fixed top-[5rem] left-0 right-0 bottom-0 lg:bg-blur lg:backdrop-blur lg:static lg:flex lg:mx-auto`}
+        >
           <div className="relative z-2 flex flex-col right-0 justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
                 key={item.id}
                 href={item.url}
-                className={`block relative font-code text-md font-bold uppercase text-n-1 transition-colors hover:text-color-1  px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === currentPathname ? "z-2 lg:text-n-1" : "lg:text-n-1/50"} lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                className={`block relative font-code text-md font-bold uppercase text-n-1 transition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === currentPathname ? "z-2 lg:text-n-1" : "lg:text-n-1/50"} lg:leading-5 lg:hover:text-n-1 xl:px-12 ${styles.hoverAnimate}`}
               >
                 {item.title}
               </a>
@@ -38,9 +43,11 @@ const Header = () => {
           </div>
         </nav>
 
-        
-
-        <Button className="ml-auto lg:hidden" px="px-3" onClick={toggleNavigation}>
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
           <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
