cf3e0953685c634701b55ca2f4e2820c4463c654
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-06-01 10:53:46 +0530

    moved gallery seperate page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import Events from '~/components/Events/Events'
-import Gallery from '~/components/Gallery/Gallery'
+
 
 function page() {
   return (
     <main className="w-full px-16 md:px-40">
         <Events/>
-        <Gallery/>
     </main>
   )
 }
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,9 @@
+import Gallery from '~/components/Gallery/Gallery'
+
+function page() {
+    return (
+      <main className="w-full px-16 md:px-40">
+          <Gallery/>
+      </main>
+    )
+  }
