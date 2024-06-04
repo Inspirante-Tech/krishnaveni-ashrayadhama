a4b3b904f7e0d8e1ace1759f21879c17f809b111
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-06-04 19:42:27 +0530

    fix:translations

--- src/components/Information/Facilities.tsx
+++ src/components/Information/Facilities.tsx
@@ -1,11 +1,13 @@
+import { getTranslations } from "next-intl/server";
 import type { FacilityType } from "~/lib/types";
 
-export default function Facilities({ facilities }: { facilities: FacilityType[] }) {
+export default async function Facilities({ facilities }: { facilities: FacilityType[] }) {
+  const t = await getTranslations("home.facilities")
   return (
     <section className="bg-secondary-50">
       <div className="content-container flex flex-col gap-8">
         <h1 className="heading text-gray-900">
-          Facilities
+          {t("heading")}
         </h1>
         <div className="flex flex-wrap justify-center gap-4 w-full">
           {facilities.map((facility, index) => (<FacilityCard
