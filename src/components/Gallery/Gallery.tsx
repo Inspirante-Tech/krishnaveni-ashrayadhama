6a6f0e4e6b649a16aa9f7c586fed9b164406aad4
Sathwik <hsathwik002@gmail.com>
2024-06-06 23:11:53 +0530

    fix:Thubmnail,Gallary/Event

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,69 +1,5 @@
-// "use client";
-// import { CircleX } from "lucide-react";
-// import Image from "next/image";
-// import { useRef, useState } from "react";
-// import { type Image as ImageType } from "./types";
-// import Photo from "./Photo";
-// import { useTranslations } from "next-intl";
-// import ThumbnailCarousel from "../ThumbnailCarousel/ThumbnailCarousel";
-// export function Gallery({ images }: { images: ImageType[] }) {
-//   const t = useTranslations("gallery");
-//   const [selectedImage, setSelectedImage] = useState<ImageType | null>(null);
-//   const dialogRef = useRef<HTMLDialogElement>(null);
-
-//   const onSelect = (image: ImageType) => {
-//     setSelectedImage(image);
-//     dialogRef.current && dialogRef.current.showModal();
-//   };
-
-//   const onClose = () => {
-//     dialogRef.current && dialogRef.current.close();
-//   };
-//   const thumbnails = images.map(image => ({
-//     id: image.id,
-//     image: image.image,
-//     title: image.alt,
-//     alt: image.alt,
-//   }));
-
-//   return (
-//     <section className="my-4 content-container">
-//       <h2 className="font-bold text-2xl mb-4">{t("heading")}</h2>
-//       <div className="columns-1 sm:columns-2 lg:columns-4 gap-4 space-y-4">
-//         {images.map((image) => (
-//           <Photo
-//             key={image.id}
-//             url={image.image}
-//             alt={image.alt}
-//             callback={() => onSelect(image)}
-//           />
-//         ))}
-//       </div>
-
-//       {/* photodialog class refer global.css */}
-//       <dialog
-//         ref={dialogRef}
-//         className="w-[80%] h-[80%] bg-primary-200 rounded  photodialog"
-//         onClick={onClose}
-//       >
-//         <ThumbnailCarousel
-//           thumbnails={thumbnails}
-//           onThumbnailClick={onSelect}
-//         />
-
-//         <form method="dialog" className="absolute top-0 right-0 z-10">
-//           <button className="m-4" onClick={onClose}>
-//             <CircleX className="text-red-500" size={32} />
-//           </button>
-//         </form> *
-//       </dialog>
-//     </section>
-//   );
-// }
-
 "use client";
 import { CircleX } from "lucide-react";
-import Image from "next/image";
 import { useRef, useState } from "react";
 import { type Image as ImageType } from "./types";
 import Photo from "./Photo";
@@ -72,18 +8,18 @@ import ThumbnailCarousel from "../ThumbnailCarousel/ThumbnailCarousel";
 
 export function Gallery({ images }: { images: ImageType[] }) {
   const t = useTranslations("gallery");
-  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
+  const [selectedImage, setSelectedImage] = useState<ImageType | null>(null);
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   const onSelect = (image: ImageType) => {
-    const index = images.findIndex((img) => img.id === image.id);
-    setSelectedImageIndex(index);
+    setSelectedImage(image);
     dialogRef.current && dialogRef.current.showModal();
   };
 
   const onClose = () => {
+    setSelectedImage(null);
+    
     dialogRef.current && dialogRef.current.close();
-    setSelectedImageIndex(null); // Reset selected image index
   };
 
   const thumbnails = images.map((image) => ({
@@ -94,9 +30,10 @@ export function Gallery({ images }: { images: ImageType[] }) {
   }));
 
   return (
-    <section className="my-4 content-container">
-      <h2 className="font-bold text-2xl mb-4">{t("heading")}</h2>
-      <div className="columns-1 sm:columns-2 lg:columns-4 gap-4 space-y-4">
+    <section className="my-4 content-container bg-white">
+      <h2 className="font-bold text-3xl md:text-5xl mb-4">{t("heading")}</h2>
+      <hr/>
+      <div className="grid grid-cols-1 mt-2  sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {images.map((image) => (
           <Photo
             key={image.id}
@@ -106,25 +43,26 @@ export function Gallery({ images }: { images: ImageType[] }) {
           />
         ))}
       </div>
-
-      {/* photodialog class refer global.css */}
       <dialog
         ref={dialogRef}
-        className="w-[80%] h-[80%] bg-primary-200 rounded photodialog"
+        className="w-[80%] h-[100%] p-2 md:p-15  bg-gray-100 rounded-xl  eventdialog overflow-hidden"
         onClick={onClose}
-        onScroll={e=>e.stopPropagation()}
       >
-        <div className="w-full h-full" onClick={e=>e.stopPropagation()}>
-        <ThumbnailCarousel
-          thumbnails={thumbnails}
-          onThumbnailClick={onSelect}
-        />
-
-        <form method="dialog" className="absolute top-0 right-0 z-10">
-          <button className="m-4" onClick={onClose}>
-            <CircleX className="text-red-500" size={32} />
-          </button>
-        </form>
+        <div className="w-full h-full mt-12 md:mt-1" onClick={(e) => e.stopPropagation()}>
+          {selectedImage && (
+            <ThumbnailCarousel
+              thumbnails={thumbnails}
+              initialIndex={thumbnails.findIndex(
+                (thumbnail) => thumbnail.id === selectedImage.id
+              )}
+              onThumbnailClick={onSelect}
+            />
+          )}
+          <form method="dialog" className="absolute top-0 right-0 z-10">
+            <button className="m-4" onClick={onClose}>
+              <CircleX className="text-black" size={32} />
+            </button>
+          </form>
         </div>
       </dialog>
     </section>
--- src/components/ThumbnailCarousel/ThumbnailCarousel.tsx
+++ src/components/ThumbnailCarousel/ThumbnailCarousel.tsx
@@ -13,29 +13,29 @@
 
 // type PropType = {
 //   thumbnails: Thumbnail[];
-//   initialIndex?: number;
 //   options?: EmblaOptionsType;
-//   onThumbnailClick?: (thumbnail: Thumbnail) => void;
+//   initialIndex?: number;
+//   onThumbnailClick?: (image: Thumbnail) => void; // Update to accept Thumbnail object
 // };
 
-// const ThumbnailCarousel: React.FC<PropType> = ({ thumbnails, initialIndex = 0, options, onThumbnailClick }) => {
+// const ThumbnailCarousel: React.FC<PropType> = ({ thumbnails, options, initialIndex = 0, onThumbnailClick }) => {
 //   const [selectedIndex, setSelectedIndex] = useState(initialIndex);
 //   const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options);
 //   const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
 //     containScroll: 'keepSnaps',
 //     dragFree: true,
 //   });
-//   console.log(thumbnails)
+
 //   const onThumbClick = useCallback(
 //     (index: number) => {
 //       if (!emblaMainApi || !emblaThumbsApi) return;
 //       emblaMainApi.scrollTo(index);
-//       onThumbnailClick && onThumbnailClick(thumbnails[index]);
+//       onThumbnailClick && onThumbnailClick(thumbnails[index]); // Pass the clicked thumbnail to onThumbnailClick
 //     },
 //     [emblaMainApi, emblaThumbsApi, onThumbnailClick, thumbnails]
 //   );
 
-//   const onSelect = useCallback(() => {
+//   const handleSelect = useCallback(() => {
 //     if (!emblaMainApi || !emblaThumbsApi) return;
 //     setSelectedIndex(emblaMainApi.selectedScrollSnap());
 //     emblaThumbsApi.scrollTo(emblaMainApi.selectedScrollSnap());
@@ -43,22 +43,21 @@
 
 //   useEffect(() => {
 //     if (!emblaMainApi) return;
-//     emblaMainApi.scrollTo(initialIndex); // Set initial index
-//     onSelect();
-//     emblaMainApi.on('select', onSelect).on('reInit', onSelect);
-//   }, [emblaMainApi, initialIndex, onSelect]);
+//     handleSelect();
+//     emblaMainApi.scrollTo(initialIndex);
+//     emblaMainApi.on('select', handleSelect).on('reInit', handleSelect);
+//   }, [emblaMainApi, handleSelect, initialIndex]);
 
 //   return (
 //     <div className={styles.embla}>
 //       <div className={styles.embla__viewport} ref={emblaMainRef}>
-//         {/* Main Image component */}
 //         <div className={styles.embla__container}>
 //           {thumbnails.map((thumbnail, index) => (
-//             <div key={thumbnail.id} className={styles.embla__slide}>
+//             <div key={thumbnail.id} className={styles.embla__slide} onClick={() => onThumbClick(index)}>
 //               <Image
 //                 src={thumbnail.image}
-//                 width={500}
-//                 height={500}
+//                 width={400}
+//                 height={400}
 //                 alt={thumbnail.alt}
 //                 className="rounded-t aspect-[1.2]"
 //               />
@@ -69,17 +68,11 @@
 //           ))}
 //         </div>
 //       </div>
-//       {/* Thumbnail component */}
 //       <div className={styles.emblaThumbs}>
 //         <div className={styles.emblaThumbs__viewport} ref={emblaThumbsRef}>
 //           <div className={styles.emblaThumbs__container}>
 //             {thumbnails.map((thumbnail, index) => (
-//               <div 
-//                 key={thumbnail.id} 
-//                 onClick={() => onThumbClick(index)} 
-//                 onMouseDown={(e) => e.stopPropagation()}  // Stop event propagation
-//                 className={`${styles.emblaThumbs__slide} ${index === selectedIndex ? styles.emblaThumbs__slide__selected : ''} `}
-//               >
+//               <div key={thumbnail.id} onClick={() => onThumbClick(index)} className={`${styles.emblaThumbs__slide} ${index === selectedIndex ? styles.emblaThumbs__slide__selected : ''} `}>
 //                 <Image
 //                   src={thumbnail.image}
 //                   width={100}
@@ -97,6 +90,8 @@
 // };
 
 // export default ThumbnailCarousel;
+
+
 import React, { useState, useEffect, useCallback } from 'react';
 import { EmblaOptionsType } from 'embla-carousel';
 import useEmblaCarousel from 'embla-carousel-react';
@@ -112,12 +107,12 @@ type Thumbnail = {
 
 type PropType = {
   thumbnails: Thumbnail[];
-  initialIndex?: number;
   options?: EmblaOptionsType;
+  initialIndex?: number;
   onThumbnailClick?: (thumbnail: Thumbnail) => void;
 };
 
-const ThumbnailCarousel: React.FC<PropType> = ({ thumbnails, initialIndex = 0, options, onThumbnailClick }) => {
+const ThumbnailCarousel: React.FC<PropType> = ({ thumbnails, options, initialIndex = 0, onThumbnailClick }) => {
   const [selectedIndex, setSelectedIndex] = useState(initialIndex);
   const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options);
   const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
@@ -129,62 +124,56 @@ const ThumbnailCarousel: React.FC<PropType> = ({ thumbnails, initialIndex = 0, o
     (index: number) => {
       if (!emblaMainApi || !emblaThumbsApi) return;
       emblaMainApi.scrollTo(index);
+      setSelectedIndex(index);
       onThumbnailClick && onThumbnailClick(thumbnails[index]);
     },
-    [emblaMainApi, emblaThumbsApi, onThumbnailClick, thumbnails]
+    [emblaMainApi, emblaThumbsApi, thumbnails, onThumbnailClick]
   );
 
-  const onSelect = useCallback(() => {
+  const handleSelect = useCallback(() => {
     if (!emblaMainApi || !emblaThumbsApi) return;
     setSelectedIndex(emblaMainApi.selectedScrollSnap());
     emblaThumbsApi.scrollTo(emblaMainApi.selectedScrollSnap());
-  }, [emblaMainApi, emblaThumbsApi, setSelectedIndex]);
+  }, [emblaMainApi, emblaThumbsApi]);
 
   useEffect(() => {
     if (!emblaMainApi) return;
-    emblaMainApi.scrollTo(initialIndex); // Set initial index
-    onSelect();
-    emblaMainApi.on('select', onSelect).on('reInit', onSelect);
-  }, [emblaMainApi, initialIndex, onSelect]);
+    handleSelect();
+    emblaMainApi.scrollTo(initialIndex);
+    emblaMainApi.on('select', handleSelect).on('reInit', handleSelect);
+  }, [emblaMainApi, handleSelect, initialIndex]);
 
   return (
     <div className={styles.embla}>
       <div className={styles.embla__viewport} ref={emblaMainRef}>
-        {/* Main Image component */}
         <div className={styles.embla__container}>
           {thumbnails.map((thumbnail, index) => (
-            <div key={thumbnail.id} className={styles.embla__slide}>
+            <div key={thumbnail.id} className={styles.embla__slide} onClick={() => onThumbClick(index)}>
               <Image
                 src={thumbnail.image}
                 width={500}
                 height={500}
                 alt={thumbnail.alt}
-                className="rounded-t  object-contain"
+                className={`rounded-t aspect-[1.2]  ${index === selectedIndex ? styles.selected : ''}`}
               />
               <div className="p-2">
-                <h2 className="text-xl">{thumbnail.title}</h2>
+                <h2 className="text-2xl font-bold">{thumbnail.title}</h2>
               </div>
             </div>
           ))}
         </div>
       </div>
-      {/* Thumbnail component */}
       <div className={styles.emblaThumbs}>
         <div className={styles.emblaThumbs__viewport} ref={emblaThumbsRef}>
           <div className={styles.emblaThumbs__container}>
             {thumbnails.map((thumbnail, index) => (
-              <div 
-                key={thumbnail.id} 
-                onClick={() => onThumbClick(index)} 
-                onMouseDown={(e) => e.stopPropagation()}  // Stop event propagation
-                className={`${styles.emblaThumbs__slide} ${index === selectedIndex ? styles.emblaThumbs__slide__selected : ''} `}
-              >
+              <div key={thumbnail.id} onClick={() => onThumbClick(index)} className={`${styles.emblaThumbs__slide} ${index === selectedIndex ? styles.emblaThumbs__slide__selected : ''} `}>
                 <Image
                   src={thumbnail.image}
-                  width={100}
-                  height={100}
+                  width={50}
+                  height={50}
                   alt={thumbnail.alt}
-                  className="rounded object-cover aspect-square"
+                  className="rounded aspect-[1.2] object-cover "
                 />
               </div>
             ))}
@@ -196,4 +185,3 @@ const ThumbnailCarousel: React.FC<PropType> = ({ thumbnails, initialIndex = 0, o
 };
 
 export default ThumbnailCarousel;
-
