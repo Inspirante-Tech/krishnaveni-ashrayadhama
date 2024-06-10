b0eb30ff2f0828806b3f506ba0e8e16d5668e8ec
Nandan Pai <nandanrp09@gmail.com>
2024-06-10 19:27:08 +0530

    [fix] Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,8 +11,8 @@ async function Footer() {
       <div className="bg-secondary-900 h-1/2 w-full ">
         <div className="content-container flex md:flex-row flex-col justify-around items-start">
           <div className="p-5 ">
-            <ul>
-              <div className="flex gap-2 bg-white rounded-xl">
+            <ul className="flex flex-col justify-center items-center gap-4">
+              <div className="flex gap-2 bg-primary-100 rounded-full justify-between items-center px-4 py-2">
                 <Image
                   src={"/logo.png"}
                   height={100}
@@ -28,12 +28,6 @@ async function Footer() {
                   className="object-center object-contain"
                 />
               </div>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d970.869550119516!2d74.7761448!3d13.2580298!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbcb1390d9b0a71%3A0x130bf0a7a489d0dc!2sSalmara%20House!5e0!3m2!1sen!2sin!4v1718015280201!5m2!1sen!2sin"
-                style={{ border: 0 }}
-                className="rounded-lg"
-                aria-hidden="false"
-              ></iframe>
               <div className="flex gap-6 pb-5">
                 <Instagram className="text-2xl cursor-pointer transition-colors duration-150 hover:text-secondary-300" />
                 <Twitter className="text-2xl cursor-pointer transition-colors duration-150 hover:text-secondary-300" />
@@ -42,13 +36,19 @@ async function Footer() {
               </div>
             </ul>
           </div>
-          <div className="p-5">
-            <p className="heading pb-4 !text-gray-100">Address</p>
-            <ul className="">
-              <li className="text-gray-300 body pb-2 font-semibold transition-colors duration-150 hover:text-secondary-300 cursor-pointer">
-                Lorem ipsum
-              </li>
-            </ul>
+          <div className="flex flex-col p-5 gap-4 ">
+            <p className="heading !text-gray-100">Address</p>
+            <p className="">
+              Salmara House,
+              <br /> P O Shankerpura, Kaup Tq,
+              <br /> Udupi Dist. - 574115.
+            </p>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d970.869550119516!2d74.7761448!3d13.2580298!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbcb1390d9b0a71%3A0x130bf0a7a489d0dc!2sSalmara%20House!5e0!3m2!1sen!2sin!4v1718015280201!5m2!1sen!2sin"
+              style={{ border: 0 }}
+              className="rounded-lg"
+              aria-hidden="false"
+            ></iframe>
           </div>
           <div className="p-5 flex flex-col gap-1">
             <p className="heading mb-4 !text-gray-100">{t("heading")}</p>
