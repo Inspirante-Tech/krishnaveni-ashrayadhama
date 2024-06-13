5a587f21e3ada79f82c8f8e086c07a35e3ec2aa6
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-06-13 10:06:57 +0530

    kannada translations

--- src/app/(main)/[locale]/page.tsx
+++ src/app/(main)/[locale]/page.tsx
@@ -5,36 +5,39 @@ import Facilities from "~/components/Information/Facilities";
 import Testimonials from "~/components/Testimonials/page";
 import Fqas from "~/components/Fqas/Fqas";
 import { fetchHomePage } from "~/lib/queries";
-import { getLocale } from "next-intl/server";
-import Link from "next/link";
+import { getLocale, getTranslations } from "next-intl/server";
 import { FileDown, Link as LinkIcon } from "lucide-react";
 import LocaleLink from "~/components/ui/LocaleLink";
 
 export default async function Home() {
   const locale = await getLocale();
   const data = await fetchHomePage(locale);
+
+  const t = await getTranslations("home");
   return (
     <>
       <main className="space-y-8">
         <Hero carouselImages={data.carosuel} />
         <section className="content-container">
           <div >
-            <h1 className="heading pb-4">Resources</h1>
-            <p className="pb-8">quick links for anyone interested in our services</p>
+            <h1 className="heading pb-4">{t("resources.heading")}</h1>
+            <p className="pb-8">{t("resources.content")}</p>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="bg-gradient-to-br from-secondary-300 to-secondary-200/60  hover:shadow-2xl transition-shadow duration-300 p-4 rounded-l-2xl">
-              <h2 className="subheading py-4">Forms</h2>
-              <ul>
-                <li className="flex gap-2 items-center"> <FileDown className="inline" size={16} /> <a href="/pre_admission_rules.pdf">Pre admission rules</a></li>
+              <h2 className="subheading py-4">{t("resources.forms.heading")}</h2>
+              <ul className="space-y-2">
+                {/* <li className="flex gap-2 items-center"> <FileDown className="inline" size={16} /> <a href="/pre_admission_rules.pdf">Pre admission rules</a></li> */}
+                <li className="flex gap-2 items-center"> <FileDown className="inline" size={16} /> <a href="/Nomination form.pdf">{t("resources.forms.nominationForm")}</a></li>
+                <li className="flex gap-2 items-center"> <FileDown className="inline" size={16} /> <a href="/Permanent form.pdf">{t("resources.forms.permanentForm")}</a></li>
               </ul>
             </div>
             <div className="bg-gradient-to-bl from-primary-300 to-primary-200/80  hover:shadow-2xl transition-shadow duration-300  p-4 rounded-r-2xl">
-              <h2 className="subheading py-4">Links</h2>
-              <ul className="space-y-4">
-                <li className="flex gap-2 items-center transition-all duration-300 ease-in-out"> <LinkIcon size={16} /><LocaleLink href="/ashraya-dhama/pricing">Pricing</LocaleLink></li>
-                <li className="flex gap-2 items-center"> <LinkIcon size={16} /><LocaleLink href="/ashraya-dhama/rules-regulations">Rules and Regulations</LocaleLink></li>
+              <h2 className="subheading py-4">{t("resources.links.heading")}</h2>
+              <ul className="space-y-2">
+                <li className="flex gap-2 items-center transition-all duration-300 ease-in-out"> <LinkIcon size={16} /><LocaleLink href="/ashraya-dhama/pricing">{t("resources.links.pricing")}</LocaleLink></li>
+                <li className="flex gap-2 items-center"> <LinkIcon size={16} /><LocaleLink href="/ashraya-dhama/rules-regulations">{t("resources.links.rulesRegulations")}</LocaleLink></li>
               </ul>
             </div>
           </div>
