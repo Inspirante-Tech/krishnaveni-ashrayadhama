51b423314a9f5b2e4d413beff4b6611f8f6c6b6d
Sathwik <hsathwik002@gmail.com>
2024-06-15 00:54:22 +0530

    feath:career/page

--- src/components/Header/NestedLink.tsx
+++ src/components/Header/NestedLink.tsx
@@ -18,37 +18,37 @@ type NestedUrl = {
 };
 
 export default function LocalSwitcher({ urlObject }: { urlObject: NestedUrl }) {
-    const pathname = usePathname();
-    const active = urlObject.urls.some((e) => pathname.includes(e.url));
-    const t = useTranslations("links")
-    return (
-      <div
-        className={`lg:py-6 block h-min hover:text-secondary-800 hover:scale-105 transition-all duration-150 ease-linear capitalize w-full`}
-      >
-        <HoverCard openDelay={300}>
-          <HoverCardTrigger className="w-full hidden lg:block cursor-pointer">
-            <span
-              className={`w-full block  border-solid border-black multiLink ${active ? "border-b-2" : "border-none"}`}
-            >
-              {t(urlObject.heading)}
-            </span>
-          </HoverCardTrigger>
-          <HoverCardContent className="bg-primary-50/95 p-1 w-min shadow-xl">
-            <div className="flex flex-col gap-1 w-full text-center">
-              {urlObject.urls.map((url) => (
-                <LocaleLink
-                  href={url.url}
-                  key={url.id}
-                  className="p-2 min-w-max w-full body hover:bg-primary-300/50 transition-colors duration-300 rounded-md ease-linear multiLink"
-                >
-                  {t(url.id)}
-                </LocaleLink>
-              ))}
-            </div>
-          </HoverCardContent>
-        </HoverCard>
-  
-        <details className={`${styles.dialog} lg:hidden flex`}>
+  const pathname = usePathname();
+  const active = urlObject.urls.some((e) => pathname.includes(e.url));
+  const t = useTranslations("links");
+  return (
+    <div
+      className={`lg:py-6 block h-min hover:text-secondary-800 hover:scale-105 transition-all duration-150 ease-linear capitalize w-full`}
+    >
+      <HoverCard openDelay={300}>
+        <HoverCardTrigger className="w-full hidden lg:block cursor-pointer">
+          <span
+            className={`w-full block  border-solid border-black multiLink ${active ? "border-b-2" : "border-none"}`}
+          >
+            {t(urlObject.heading)}
+          </span>
+        </HoverCardTrigger>
+        <HoverCardContent className="bg-primary-50/95 p-1 w-min shadow-xl">
+          <div className="flex flex-col gap-1 w-full text-center">
+            {urlObject.urls.map((url) => (
+              <LocaleLink
+                href={url.url}
+                key={url.id}
+                className="p-2 min-w-max w-full body hover:bg-primary-300/50 transition-colors duration-300 rounded-md ease-linear multiLink"
+              >
+                {t(url.id)}
+              </LocaleLink>
+            ))}
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+
+      <details className={`${styles.dialog} lg:hidden flex`}>
           <summary
             className={`${styles.summary} multiLink flex flex-row gap-2 items-center justify-center`}
           >
@@ -69,6 +69,8 @@ export default function LocalSwitcher({ urlObject }: { urlObject: NestedUrl }) {
             ))}
         </div>
       </details>
+    
+
     </div>
   );
 }
