d78126e727b84c714490b2eaf5831f11a32ae13e
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2024-06-30 11:53:41 +0530

    feat: added announcement marquee

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,22 +1,25 @@
-"use client"
+"use client";
 import Carousel from "./Carousel";
 import ScrollDown from "./ScrollDown";
 import Image from "next/image";
 import LocaleLink from "../ui/LocaleLink";
-import  "./styles.module.css";
+import "./styles.module.css";
 import { Button } from "../ui/button";
 import { useTranslations } from "next-intl";
+import Marquee from "react-fast-marquee";
+import { BadgeAlert } from "lucide-react";
+import Link from "next/link";
 
 function Hero({ carouselImages }: { carouselImages: string[] }) {
-  const t= useTranslations("home.hero")
+  const t = useTranslations("home.hero");
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-full mb-14">
       <Carousel>
         {carouselImages.map((url, index) => (
           <div key={index} className="relative w-full h-full">
             <Image
               width={1000}
-              height={Math.floor(1000*9/16)}
+              height={Math.floor((1000 * 9) / 16)}
               className="min-w-[100vw] h-full object-cover snap-center"
               src={url}
               alt={`Slide ${index + 1}`}
@@ -29,12 +32,12 @@ function Hero({ carouselImages }: { carouselImages: string[] }) {
           <div className="flex flex-col gap-2 md:gap-4">
             <h1 className="title">{t("heading")}</h1>
             <p className="text-gray-200 subheading leading-relaxed text-center">
-            {t("content")}
+              {t("content")}
             </p>
           </div>
           <LocaleLink href="/contact" className="pointer-events-auto">
             <Button className="body h-12 px-6 rounded-lg bg-primary-700 hover:bg-primary-600">
-            {t("cta")}
+              {t("cta")}
             </Button>
           </LocaleLink>
         </div>
@@ -42,7 +45,38 @@ function Hero({ carouselImages }: { carouselImages: string[] }) {
       <div
         className={`absolute bg-gradient-to-t from-black/75 from-25% to-transparent w-full h-3/5 bottom-0 pointer-events-none`}
       ></div>
-      <ScrollDown targetId="story"/>
+      <ScrollDown targetId="story" />
+
+      <div className="bg-secondary-200 py-4 flex flex-col gap-4 justify-center absolute top-full w-full">
+        <Marquee className="body" pauseOnHover>
+          <span className="flex gap-2 items-center mx-4 md:mx-6 font-semibold">
+            <BadgeAlert size={24} color="#8a0000" />
+            <span>
+              Discount available for all the treatements for one month from
+              15/07/2024,{" "}
+              <Link
+                href={"/en/ayurvedic-center/therapy-tariff"}
+                className="underline"
+              >
+                Check out here
+              </Link>
+            </span>
+          </span>
+          <span className="flex gap-2 items-center mx-4 md:mx-6 font-semibold">
+            <BadgeAlert size={24} color="#8a0000" />
+            <span>
+              Discount available for all the treatements for one month from
+              15/07/2024,{" "}
+              <Link
+                href={"/en/ayurvedic-center/therapy-tariff"}
+                className="underline"
+              >
+                Check out here
+              </Link>
+            </span>
+          </span>
+        </Marquee>
+      </div>
     </div>
   );
 }
--- src/components/Information/Story.tsx
+++ src/components/Information/Story.tsx
@@ -1,37 +1,43 @@
-import Image from 'next/image';
-import Reveal from '../Animations/reveal';
-import ScrollLag from '../Animations/scrollLag';
-import Heading from '../Heading/Heading';
-import RichText from '../RichText/RichText';
-import { SectionType } from '~/lib/types';
+import Image from "next/image";
+import Reveal from "../Animations/reveal";
+import ScrollLag from "../Animations/scrollLag";
+import Heading from "../Heading/Heading";
+import RichText from "../RichText/RichText";
+import { SectionType } from "~/lib/types";
+import Marquee from "react-fast-marquee";
+import { BadgeAlert, Megaphone } from "lucide-react";
+import Link from "next/link";
 
 const Story: React.FC<{ data: SectionType }> = ({ data }) => {
-    return (
-        <section
-            id='story'
-            className={`relative text-justify bg-secondary-100 md:bg-transparent`}
-        >
-            <div className="h-8 bg-secondary-100 absolute bottom-full w-full md:hidden"></div>
-            <div className='content-container flex items-start'>
-                <Reveal>
-                    <div className="xl:max-w-4xl md:shadow-xl rounded-2xl">
-                        <div className="relative md:bg-gradient-to-tr md:from-secondary-200 md:to-secondary-100 rounded-2xl flex md:gap-4 gap-2 flex-col xl:p-16 lg:p-24 md:p-12">
-                            <Heading seperatorColor='secondary'>
-                                {data.title}
-                            </Heading>
+  return (
+    <section
+      id="story"
+      className={`relative text-justify bg-secondary-100 md:bg-transparent scroll-mt-14`}
+    >
+      <div className="content-container flex items-start">
+        <Reveal>
+          <div className="xl:max-w-4xl md:shadow-xl rounded-2xl">
+            <div className="relative md:bg-gradient-to-tr md:from-secondary-200 md:to-secondary-100 rounded-2xl flex md:gap-4 gap-2 flex-col xl:p-16 lg:p-24 md:p-12">
+              <Heading seperatorColor="secondary">{data.title}</Heading>
 
-                            <RichText value={data.description} />
-                        </div>
-                    </div>
-                </Reveal>
-                <Reveal delay={0.35}>
-                    <ScrollLag speed={50}>
-                        <Image src={data.image} alt='Story' height={500} width={500} className='w-[26rem] object-contain -translate-x-6 top-10 relative rounded-2xl hidden xl:block z-10 shadow-xl'></Image>
-                    </ScrollLag>
-                </Reveal>
+              <RichText value={data.description} />
             </div>
-        </section>
-    );
+          </div>
+        </Reveal>
+        <Reveal delay={0.35}>
+          <ScrollLag speed={50}>
+            <Image
+              src={data.image}
+              alt="Story"
+              height={500}
+              width={500}
+              className="w-[26rem] object-contain -translate-x-6 top-10 relative rounded-2xl hidden xl:block z-10 shadow-xl"
+            ></Image>
+          </ScrollLag>
+        </Reveal>
+      </div>
+    </section>
+  );
 };
 
 export default Story;
